44e58e1c507b60f253409551f24658321896ba1e
Abid Hasan <144882936+abid450@users.noreply.github.com>
2023-09-29 11:44:03 +0600

    I have a upload programming c file

---
+++ main.c
@@ -0,0 +1,41 @@
+
+
+
+
+
+/* Lower Cach to Upper cach ------------ */
+
+
+/*#include<stdio.h>
+  int main(){
+
+  
+  char lower,upper;
+
+  printf("Enter any lowercase : ");
+  scanf("%c",&lower);
+
+  upper = toupper(lower);
+  printf("uppercase letter = %c",upper);
+
+
+}*/
+
+
+  /* Upper to Lower case ------------- */
+
+
+ /* #include<stdio.h>
+
+    int main(){
+
+    char lower,upper;
+
+    printf("Enter a uppercase : ");
+    scanf("%c",&upper);
+
+    lower = tolower (upper);
+    printf("lower letter = %c",lower);
+
+  }*/
+
